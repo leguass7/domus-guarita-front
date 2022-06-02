a44f68260e04bdefd34f1f97c8072e0e308911f6
Leandro Sbrissa <leandro.sbrissa@hotmail.com>
2022-06-02 12:17:21 -0300

    test: teste de atualização

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,9 @@
 import { Button } from "ui";
 
 export default function Web() {
-  const sasdd = "";
-
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web 1</h1>
       <Button />
     </div>
   );
